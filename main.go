9f742b55f0e066f4e12b27e93c4793a830963e94
Daniil Makhov <mahovdnl@gmail.com>
2025-07-10 12:21:06 +0300

    added ai api

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-    "github.com/gin-gonic/gin"
+	"io"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-    "net/http"
 )
 
 func main() {
     // Создаем новый экземпляр роутера
     r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 
 	err := godotenv.Load()
 	if err != nil {
@@ -19,7 +33,6 @@ func main() {
 	}
 
 	apiKey := os.Getenv("API_KEY")
-
 	fmt.Println("API Key:", apiKey)
 
     // Определяем маршрут для главной страницы
@@ -49,6 +62,62 @@ func main() {
 			"message": "Пользователь получен",
 			"user":    user,
 		})
+	})
+
+	type Message struct{
+		Message string `json"message"`
+	}
+
+	r.POST("/api/message", func(c *gin.Context){
+		var msg Message
+		
+		if err := c.ShouldBindJSON(&msg); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		//{"chat":      {"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"hello"}]}        }
+		postBody, _ := json.Marshal(map[string]interface{}{
+			"chat": map[string]interface{}{
+				"model": "gpt-3.5-turbo",
+				"messages": []map[string]string{
+					{
+						"role":    "user",
+						"content": msg.Message,
+					},
+				},
+			},
+		})
+		requestBody := bytes.NewBuffer(postBody)
+
+		req, err := http.NewRequest(
+			"POST", 
+			"https://caila.io/api/mlpgate/account/just-ai/model/openai-proxy/predict",
+			requestBody,
+		)
+		if err != nil {
+			log.Fatalf("Error creating request: %v", err)
+		}
+
+		req.Header.Set("MLP-API-KEY", apiKey)  
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+		}
+		defer resp.Body.Close()
+		//Read the response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		sb := string(body)
+		log.Printf(sb)
+
+
+		c.JSON(http.StatusOK, sb)
 	})
 
     // Запускаем сервер на порту 8080
